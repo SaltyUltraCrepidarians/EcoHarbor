8fc3bcc44f93abd8f13378a7528ef7d37ab9ef3e
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-09-05 11:58:22 +0200

    [fix] submitting form

--- app/make-offer/page.tsx
+++ app/make-offer/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import './make-offer.css';
 
@@ -7,16 +8,31 @@ const MakeOffer = () => {
     description: '',
   });
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setOfferInfo((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('Offer Info for JOE', offerInfo)
+  }
+
   return (
     <>
-      <form className="make-offer-form">
-        <label htmlFor="">Enter the name of your company</label>
+      <form  onSubmit={handleSubmit} className="make-offer-form">
+        <label>Enter the name of your company</label>
+        <input type="text" placeholder="Company name..."
+        name="companyName" onChange={handleChange} value={offerInfo.companyName}
+        />
 
-        <input type="text" placeholder="Company name..." />
-
-        <label htmlFor="">Description</label>
-
-        <textarea name="" id="" cols={30} rows={10}></textarea>
+        <label>Description</label>
+        <textarea
+        name="description" onChange={handleChange} value={offerInfo.description}
+        cols={30} rows={10}
+        ></textarea>
 
         <button>Submit</button>
       </form>
