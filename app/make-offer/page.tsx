3e6f0a13b4c56d2e77ad9ac7473f0be48631fb41
Diana <diana.borro.dev@gmail.com>
2023-09-05 11:39:58 +0200

    Feat: MakeOffer form

--- app/make-offer/page.tsx
+++ app/make-offer/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './make-offer.css';
 
 const MakeOffer = () => {
-  return;
-  <></>;
+  const [offerInfo, setOfferInfo] = useState({
+    companyName: '',
+    description: '',
+  });
+
+  return (
+    <>
+      <form className="make-offer-form">
+        <label htmlFor="">Enter the name of your company</label>
+
+        <input type="text" placeholder="Company name..." />
+
+        <label htmlFor="">Description</label>
+
+        <textarea name="" id="" cols={30} rows={10}></textarea>
+
+        <button>Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default MakeOffer;
