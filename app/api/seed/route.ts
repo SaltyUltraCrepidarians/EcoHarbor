45463fdc376a1a739860b69861e8da412291f882
Diana <diana.borro.dev@gmail.com>
2023-09-05 15:49:11 +0200

    [fixing] offerInfoData

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,20 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import { log } from 'console';
+import { off } from 'process';
 
 const prisma = new PrismaClient();
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { offerInfo } = req.body;
+  // console.log(offerInfo);
 
-      const { companyName, description } = req.body;
-      console.log(companyName,description)
+  const offerInfoData = await JSON.parse(offerInfo);
+  console.log('offerInfo prsed', offerInfoData);
 
-       await prisma.donationInfo.create({
-        data: {
-          companyName: companyName,
-          description : description,
-        },
-      });
-      return new Response ('whatever')
-  }
+  await prisma.donationInfo.create({
+    data: {
+      companyName: offerInfoData.companyName,
+      description: offerInfoData.description,
+    },
+  });
+  return new Response('whatever');
+}
 
 
