a490c6d2865a87919f4462d1914740bcd5ce7d98
Alexandra Macovei <macovei.a2@gmail.com>
2023-09-08 10:08:01 +0200

    [fix] making the account component

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react';
 
 export default function page() {
+  const [selection, setSelection] = useState('profile');
+
+  const handleSelection = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelection(e.currentTarget.name);
+    console.log('this is the selection', e.currentTarget.name);
+  };
   return (
     <div>
-      hello there !!! didi && Gusti
+      <button onClick={handleSelection} name="profile">
+        Profile
+      </button>
+      <button onClick={handleSelection} name="form">
+        Form
+      </button>
+      <button onClick={handleSelection} name="offers">
+        My Offers
+      </button>
     </div>
-  )
+  );
 }
