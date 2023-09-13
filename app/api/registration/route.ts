4cb1ef6fe8748b4f1444f937e7c8145f6817a28a
Diana <diana.borro.dev@gmail.com>
2023-09-13 15:08:26 +0200

    [feat] fixing registration fetch

---
+++ app/api/registration/route.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { authHandler } from '../auth/[...nextauth]/route';
+
+const prisma = new PrismaClient();
+
+// POST ONE OFFER CARD
+export async function POST(req: NextRequest, res: NextResponse) {
+  const registrationInfo = await req.text();
+  const registrationInfoData = await JSON.parse(registrationInfo);
+
+  const session = await getServerSession(authHandler);
+
+  console.log('HAPPY REG INFO DATA', registrationInfoData);
+
+  if (session?.user?.email) {
+    await prisma.userInfo.update({
+      where: {
+        personalEmail: session?.user?.email,
+      },
+      data: {
+        businessName: registrationInfoData.businessName,
+        businessEmail: registrationInfoData.businessEmail,
+        businessPhoneNr: registrationInfoData.businessPhoneNr,
+        businessImage: registrationInfoData.businessImage,
+        businessAdress: registrationInfoData.businessAdress,
+      },
+    });
+    return new Response('Posted to donationInfo');
+  }
+}
+
+// GET ONE OFFER CARD
--- app/registration/Components/Registration.tsx
+++ app/registration/Components/Registration.tsx
@@ -1,36 +1,97 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import './Registration.css';
+import { defaultRegistrationValues } from '@/app/account/AccountComponents/makeOfferDefaultValues';
 
 export default function Registration() {
+  const [registrationInfo, setRegistrationInfo] = useState(
+    defaultRegistrationValues
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRegistrationInfo((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Registration info', registrationInfo);
+    try {
+      const res = await fetch('/api/registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(registrationInfo),
+      });
+
+      setRegistrationInfo(defaultRegistrationValues);
+      return res.text;
+    } catch (err) {
+      console.error('Failed to fetch data', err);
+    }
+  };
+
   return (
     <section className="make-offer-wrapper">
-      <form className="make-offer-form">
+      <form className="make-offer-form" onSubmit={handleSubmit}>
         <p className="title"> Register</p>
 
         <div className="label-input-wrap">
           <label>Business Name</label>
-          <input type="text" name="" id="" required />
+          <input
+            type="text"
+            name="businessName"
+            onChange={handleChange}
+            value={registrationInfo.businessName}
+            required
+          />
         </div>
 
         <div className="label-input-wrap">
           <label>Contact Email</label>
-          <input type="text" name="" id="" required />
+          <input
+            type="text"
+            name="businessEmail"
+            onChange={handleChange}
+            value={registrationInfo.businessEmail}
+            required
+          />
         </div>
 
         <div className="label-input-wrap">
           <label>Contact Number</label>
-          <input type="text" required />
+          <input
+            type="text"
+            name="businessPhoneNr"
+            onChange={handleChange}
+            value={registrationInfo.businessPhoneNr}
+            required
+          />
         </div>
 
         <div className="label-input-wrap">
           {/* should be an upload */}
           <label> Business Image</label>
-          <input type="text" name="" id="" />
+          <input
+            type="text"
+            name="businessImage"
+            onChange={handleChange}
+            value={registrationInfo.businessImage}
+          />
         </div>
 
         <div className="label-input-wrap">
           <label>Business Adress</label>
-          <input type="text" name="" id="" required />
+          <input
+            type="text"
+            name="businessAdress"
+            onChange={handleChange}
+            value={registrationInfo.businessAdress}
+            required
+          />
         </div>
         <button className="account-button">Submit</button>
       </form>
